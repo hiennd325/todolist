eb2d4d93a049373a4533f4e9e3747aad8a64aeac
hiennd325 <hiennd325@gmail.com>
2026-03-30 15:30:31 +0700

    fix add reminder

--- app/src/main/java/com/example/todolist/ui/components/AddEditTodoDialog.kt
+++ app/src/main/java/com/example/todolist/ui/components/AddEditTodoDialog.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Alarm
@@ -143,7 +145,7 @@ fun AddEditTodoDialog(
             }
         },
         text = {
-            Column {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                 OutlinedTextField(
                     value = title,
                     onValueChange = {
